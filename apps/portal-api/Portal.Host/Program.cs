a41a7f883aa2ad000c0072aec8be44bdd3c6b42f
Paul Jones <paul.j@smartsurvey.co.uk>
2025-10-14 10:04:15 +0100

    Add simple comms

--- apps/portal-api/Portal.Host/Program.cs
+++ apps/portal-api/Portal.Host/Program.cs
@@ -6,7 +6,16 @@ using Portal.Application.Weather;
 using Portal.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
 builder.Services.AddControllers();
+builder.Services.AddCors(o => o.AddDefaultPolicy(policy =>
+{
+    var origins = config.GetSection("Cors:Origins").Get<string[]>()!;
+    _ = policy.WithOrigins(origins)
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IForecastRepo, InMemForecastRepo>();
@@ -14,6 +23,7 @@ builder.Services.AddScoped<IForecastService, ForecastService>();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
